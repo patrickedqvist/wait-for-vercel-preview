fd45ee57c70618f903439dac4103bd3c0afcc074
Patrick Edqvist <patrick.edqvist@gmail.com>
2021-02-15 16:31:45 +0100

    update lib/dist settings

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,9 +1,8 @@
-/* eslint-disable no-console */
-
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+
+import {wait} from '../src/wait'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
@@ -14,17 +13,6 @@ test('wait 500 ms', async () => {
   const start = new Date()
   await wait(500)
   const end = new Date()
-  const delta = Math.abs(end.getTime() - start.getTime())
+  var delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'dist', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
 })

859721c8a58e749a9b6489390535635575a329d9
Josh Barr <josh.barr@stedi.com>
2022-01-14 06:36:08 +1300

    test: add coverage reporting

--- test/support/server.js
+++ test/support/server.js
@@ -3,8 +3,5 @@ const { rest } = require('msw');
 
 // This configures a request mocking server with the given request handlers.
 exports.server = setupServer();
-// rest.get('https://github.com/*', (req, res, ctx) => {
-//   return res(ctx.json({ firstName: 'John' }));
-// })
 
 exports.rest = rest;
